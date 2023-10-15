5867b03f451a88786103be608b6f664a1ab3d85a
Shane Martin <shane.martin@plooto.com>
2023-10-15 11:24:19 -0400

    feat: init new Violentmonkey project

---
+++ src/meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        My Script
+// @namespace   Violentmonkey Scripts
+// @description This is a userscript.
+// @match       *://*/*
+// @grant       GM_addStyle
+// @version     process.env.VERSION
+// @author      process.env.AUTHOR
+// @require     https://cdn.jsdelivr.net/combine/npm/@violentmonkey/dom@2,npm/@violentmonkey/ui@0.7
+// @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2/dist/solid.js
+// ==/UserScript==
+
+/**
+ * Code here will be ignored on compilation. So it's a good place to leave messages to developers here.
+ *
+ * - The `@grant`s used in your source code will be added automatically by `rollup-plugin-userscript`.
+ *   However you have to add explicitly those used in required resources.
+ * - `process.env.VERSION` and `process.env.AUTHOR` will be loaded from `package.json`.
+ */
