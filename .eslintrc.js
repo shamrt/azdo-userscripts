85f0eea809cff4d6de9c50b29391e74b72c43534
Shane Martin <shane.martin@plooto.com>
2023-10-15 11:29:41 -0400

    test: replace Prettier ESLint with vanilla scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,5 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
   ],
 };
